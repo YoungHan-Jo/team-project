6786d40d07c8d8b431d27a4a657468cef5bc660e
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2021-10-28 09:49:05 +0900

    spring base setting

--- spring-legacy/src/main/java/com/example/controller/HomeController.java
+++ spring-legacy/src/main/java/com/example/controller/HomeController.java
@@ -3,9 +3,6 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 
