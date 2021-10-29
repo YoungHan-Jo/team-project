f5488f4691c587bedb9a96a7b73355c601802550
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2021-10-29 11:22:03 +0900

    member controller

---
+++ spring-legacy/src/main/java/com/example/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member/*")
+public class MemberController {
+
+}
