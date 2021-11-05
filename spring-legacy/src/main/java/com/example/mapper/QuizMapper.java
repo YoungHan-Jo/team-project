12608b73e6ea0bb6cd3e53ad14e8019cd4ea903b
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2021-11-05 16:16:16 +0900

    commit1105

--- spring-legacy/src/main/java/com/example/mapper/QuizMapper.java
+++ spring-legacy/src/main/java/com/example/mapper/QuizMapper.java
@@ -3,6 +3,8 @@ package com.example.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.domain.Criteria;
 import com.example.domain.BunchVO;
 import com.example.domain.QuizVO;
@@ -25,6 +27,10 @@ public interface QuizMapper {
 	SolveHistoryVO getSolveHistoryByNum(int num);
 	
 	List<QuizVO> getQuizListByResult(Map<String, Object> map);
+	
+	// 마이바티스는 매퍼 메소드의 매개변수 선언이 두개 이상일때
+	// SQL문에서 각각 사용될 이름을 명시적으로 줘야함
+	//List<QuizVO> getQuizListByResult2(@RequestParam("quiz") QuizVO quizVO, @RequestParam("s") String str);
 	
 	List<BunchVO> getBunchesById(String id);
 	
